4736463cdc24f820b5f69c5a44423c5771f74777
e-Rentav <pedrobragalima@gmail.com>
2024-08-02 14:37:38 -0300

    Organize: Retirando rotas comentadas inutilizadas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,34 +17,9 @@ use App\Http\Controllers\Students;
 
 Route::apiResource('students', Students::class);
 Route::apiResource('classes', SchoolClasses::class); 
-// Route::post('classes/create', [SchoolClasses::class, 'store']); 
-
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 /**
  * Para listar os alunos de uma turma:
  */
 // Route::prefix('students/{studentId}')->group(function () {
 //     Route::get('school-classes', [Students::class, 'schoolClasses']);
 // });
-
-/**
- * Protegendo rotas resource de acesso
- */
-    /**
-     * index  | get all
-     * create | get $id
-     * store  | post $request/$model/$id
-     * edit   | get $id
-     * update | patch $request/$model/$id
-     * delete | delete?patch $id
-     */
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::apiResource('students', Students::class);
-//     Route::apiResource('school-classes', SchoolClasses::class);    
-// });
-
-// Route::get('/students', [Students::class, 'index']);
