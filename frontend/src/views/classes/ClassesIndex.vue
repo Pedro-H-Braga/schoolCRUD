17826801543765e8a9cd9ba415275d515254b6b1
e-Rentav <pedrobragalima@gmail.com>
2024-08-04 23:23:39 -0300

    Feat: Apagar Turma | Titulo na pagina | corrigindo estilos

--- frontend/src/views/classes/ClassesIndex.vue
+++ frontend/src/views/classes/ClassesIndex.vue
@@ -2,7 +2,7 @@
 import useClasses from '@/composables/classes';
 import { onErrorCaptured, onMounted, ref, computed } from 'vue';
 
-const { classes, getClasses } = useClasses()
+const { classes, getClasses, destroyClass } = useClasses()
 
 const errorMsg = ref(null);
 const isLoading = ref(true);
@@ -27,7 +27,6 @@ onErrorCaptured(() => {
 </script>
 
 <template>
-<div v-if="errorMsg">{{ errorMsg }}</div>
 <div v-if="isLoading || isClassesEmpty">
 
 <div class="fixed inset-0 flex items-center justify-center bg-gray-50 dark:bg-gray-800">
@@ -38,13 +37,14 @@ onErrorCaptured(() => {
 
 </div>
 <div v-else>
-    <div class="mt-4 flex justify-end mb-4">
+    <div class="flex justify-between items-center mb-4 mt-4">
+        <span class="text-2xl font-semibold whitespace-nowrap dark:text-white">Turmas</span>
         <RouterLink :to="{name: 'ClassesCreate'}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Criar Turma</RouterLink>
     </div>
 
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-600 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
                         Nome da Turma
@@ -55,14 +55,13 @@ onErrorCaptured(() => {
                     <th scope="col" class="px-6 py-3">
                         Turno Escola
                     </th>
-                    <th scope="col" class="px-6 py-3">
-                        Ações
+                    <th scope="col" class="px-6 py-3 w-32">
                     </th>
                 </tr>
             </thead>
             <tbody>
-                <tr  v-for="schoolClass in classes" :key="schoolClass.id" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-700 border-b dark:border-gray-500">
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <tr v-for="schoolClass in classes" :key="schoolClass.id" class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-800 whitespace-nowrap dark:text-white">
                         {{ schoolClass.name }}
                     </th>
                     <td class="px-6 py-4">
@@ -72,13 +71,12 @@ onErrorCaptured(() => {
                         {{ schoolClass.school_term }}
                     </td>
                     <td class="px-6 py-4">
-                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    </td>
+                        <RouterLink :to="{name: 'ClassesEdit', params: {id: schoolClass.id}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</RouterLink>
+                        <button @click="destroyClass(schoolClass.id)" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remover</button>
+                    </td>                   
                 </tr>
             </tbody>
         </table>
     </div>
-
-
 </div>
 </template>
